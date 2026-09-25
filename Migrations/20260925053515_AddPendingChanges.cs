fd98f655617c0c55f763da0484ddc30e0fdbdbef
jamesmiguellll <jamesmigueltgalgana@iskolarngbayan.pup.edu.ph>
2026-09-25 19:07:30 +0800

    Implement UX improvements and standardize UI flows

---
+++ Migrations/20260925053515_AddPendingChanges.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_scm.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CommittedAt",
+                table: "StockIns",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CommittedBy",
+                table: "StockIns",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommittedAt",
+                table: "StockIns");
+
+            migrationBuilder.DropColumn(
+                name: "CommittedBy",
+                table: "StockIns");
+        }
+    }
+}
